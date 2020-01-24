bc4902e50cc8cc90b73afb8c158331ea06615790
meiuwa <44991669+meiuwa@users.noreply.github.com>
2020-01-24 16:44:42 +0800

    :arrow_up: Updated dependencies, and Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.61"
     id("com.gradle.plugin-publish") version "0.10.1"
-    `maven-publish`
 }
 
 group = "com.meiuwa"
-version = "1.0.0"
+version = "2.0.0"
 description = "Integrates Sass executable compiler with Gradle"
 
 repositories {
@@ -15,28 +14,19 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-    implementation(kotlin("stdlib-jdk8"))
-    implementation("de.undercouch:gradle-download-task:4.0.0")
+    implementation(kotlin("stdlib"))
+    implementation("de.undercouch:gradle-download-task:4.0.2")
 }
 
 pluginBundle {
     website = "https://github.com/meiuwa/gradle-sass"
     vcsUrl = "https://github.com/meiuwa/gradle-sass.git"
-
     plugins {
         create("gradle-sass") {
             id = "com.meiuwa.gradle.sass"
             displayName = "gradle-sass"
             description = "Integrates Sass executable compiler with Gradle"
             tags = listOf("sass", "scss", "css", "compiler", "web")
-        }
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("gradle-sass") {
-            from(components["java"])
         }
     }
 }
